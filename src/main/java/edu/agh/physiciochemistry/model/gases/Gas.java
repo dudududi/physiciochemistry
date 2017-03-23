d94ee30d07e5db5e0dbe8db9a478ce80d3028629
Szymon <furmanszymon@gmail.com>
2017-03-23 13:00:17 +0100

    Create gas class

---
+++ src/main/java/edu/agh/physiciochemistry/model/gases/Gas.java
@@ -0,0 +1,31 @@
+package edu.agh.physiciochemistry.model.gases;
+
+/**
+ * Created by Szymon on 23.03.2017.
+ */
+public class Gas {
+    private double moleNumber;
+    private double properHeat;
+
+    public Gas(double moleNumber, double properHeat){
+        this.moleNumber = moleNumber;
+        this.properHeat = properHeat;
+    }
+
+
+    public double getMoleNumber() {
+        return moleNumber;
+    }
+
+    public void setMoleNumber(double moleNumber) {
+        this.moleNumber = moleNumber;
+    }
+
+    public double getProperHeat() {
+        return properHeat;
+    }
+
+    public void setProperHeat(double properHeat) {
+        this.properHeat = properHeat;
+    }
+}

a67bdbe7900c531647554162e93ac3196ea0dc2f
dudududi <kamil.dudek@gmail.com>
2017-04-06 13:51:39 +0200

    Invoke JS callback from Java

--- src/main/java/edu/agh/physiciochemistry/JavaAPI.java
+++ src/main/java/edu/agh/physiciochemistry/JavaAPI.java
@@ -1,5 +1,7 @@
 package edu.agh.physiciochemistry;
 
+import netscape.javascript.JSObject;
+
 /**
  * Created by dudek on 3/23/17.
  */
@@ -8,8 +10,9 @@ public class JavaAPI {
     public static final String JS_NAME = "JavaAPI";
 
     @SuppressWarnings("unused")
-    public void count(int x, double y){
+    public void count(int x, double y, JSObject callback){
         double result = x*y;
+        callback.call("onResult", result);
         System.out.println(result);
     }
 
